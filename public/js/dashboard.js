08fa594a27827a7708a878d5ce6a91864b4b7af1
Brad Lindsay <bslindsa@gmail.com>
2022-02-03 14:49:06 -0700

    View or edit dashboard posts

--- public/js/dashboard.js
+++ public/js/dashboard.js
@@ -3,3 +3,13 @@ document
     .addEventListener('click', () => {
         document.location.replace('/newpost')
     });
+
+const toEdit = (event) => {
+    if (event.target.hasAttribute('data-id')) {
+        let id = event.target.getAttribute('data-id');
+        document.location.replace(`/dashboard/${id}`);
+    };
+};
+
+document.querySelector('.dashboard')
+    .addEventListener('click', toEdit);
